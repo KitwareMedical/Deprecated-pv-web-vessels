6f5553896cb1eb3655011935c4d73b1d09ff8cdb
Forrest Li <forrest.li@kitware.com>
2018-02-07 11:46:08 -0500

    enh(ITKImageReader): Add and register the ITK image reader

---
+++ Sources/helpers/ITKImageReader/index.js
@@ -0,0 +1,106 @@
+import macro from 'vtk.js/Sources/macro';
+import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
+import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+
+import readImageArrayBuffer from 'itk/readImageArrayBuffer';
+import Matrix from 'itk/Matrix';
+import getMatrixElement from 'itk/getMatrixElement';
+
+// ----------------------------------------------------------------------------
+
+function convertItkToVtkImage(itkImage) {
+  const array = {
+    values: itkImage.data,
+    numberOfComponents: itkImage.imageType.components,
+  };
+
+  const vtkImage = {
+    origin: [0, 0, 0],
+    spacing: [1, 1, 1],
+  };
+
+  const dimensions = [1, 1, 1];
+
+  const direction = new Matrix(3, 3);
+  direction.setIdentity();
+
+  for (let idx = 0; idx < itkImage.imageType.dimension; ++idx) {
+    vtkImage.origin[idx] = itkImage.origin[idx];
+    vtkImage.spacing[idx] = itkImage.spacing[idx];
+    dimensions[idx] = itkImage.size[idx];
+    for (let col = 0; col < itkImage.imageType.dimension; ++col) {
+      direction.setElement(
+        idx,
+        col,
+        getMatrixElement(itkImage.direction, idx, col)
+      );
+    }
+  }
+
+  // Create VTK Image Data
+  const imageData = vtkImageData.newInstance(vtkImage);
+  const scalar = vtkDataArray.newInstance(array);
+  imageData.setDirection(direction.data);
+  imageData.setDimensions(...dimensions);
+  imageData.getPointData().setScalars(scalar);
+
+  return imageData;
+}
+
+// ----------------------------------------------------------------------------
+// vtkITKImageReader methods
+// ----------------------------------------------------------------------------
+
+function vtkITKImageReader(publicAPI, model) {
+  // Set our className
+  model.classHierarchy.push('vtkITKImageReader');
+
+  // Returns a promise to signal when image is ready
+  publicAPI.parseArrayBuffer = (arrayBuffer, filename) => {
+    if (!arrayBuffer || arrayBuffer === model.rawDataBuffer) {
+      return Promise.resolve();
+    }
+
+    model.rawDataBuffer = arrayBuffer;
+    model.filename = filename;
+
+    return readImageArrayBuffer(arrayBuffer, filename).then((itkImage) => {
+      const imageData = convertItkToVtkImage(itkImage);
+      model.output[0] = imageData;
+
+      // TODO should this be here or outside the promise?
+      publicAPI.modified();
+    });
+  };
+
+  publicAPI.requestData = (inData, outData) => {
+    publicAPI.parseArrayBuffer(model.rawDataBuffer, model.filename);
+  };
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+const DEFAULT_VALUES = {};
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+
+  // Build VTK API
+  macro.obj(publicAPI, model);
+  macro.algo(publicAPI, model, 0, 1);
+
+  // vtkITKImageReader methods
+  vtkITKImageReader(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = macro.newInstance(extend, 'vtkXMLImageDataReader');
+
+// ----------------------------------------------------------------------------
+
+export default { newInstance, extend };
--- Sources/index.js
+++ Sources/index.js
@@ -1,8 +1,28 @@
 import * as Glance from 'paraview-glance';
 
+import itkExtensionToIO from 'itk/extensionToIO';
+
 import Rpc from './Rpc';
 import SegmentTubeApi from './api/SegmentTube';
 import { getBackendHostAndPort } from './ElectronUtils';
+
+import vtkITKImageReader from './helpers/ITKImageReader';
+
+function registerITKReader() {
+  // ReaderFactory will lowercase all input filenames, so remove duplicate
+  // extensions here.
+  new Set(
+    Object.keys(itkExtensionToIO).map((ext) => ext.toLowerCase())
+  ).forEach((ext) =>
+    Glance.registerReader(
+      ext,
+      `${ext.toUpperCase()} Reader`,
+      vtkITKImageReader,
+      'readAsArrayBuffer',
+      'parseArrayBuffer'
+    )
+  );
+}
 
 function main(mountPoint) {
   const [backendHost, backendPort] = getBackendHostAndPort();
@@ -12,6 +32,8 @@ function main(mountPoint) {
     port: backendPort,
   });
   rpc.connect();
+
+  registerITKReader();
 
   Glance.createViewer(mountPoint);
 }
